16e2f6bf75a97381900e9f5187fc7c849a6c5a3a
chris <49421186+unidentifiedchris@users.noreply.github.com>
2019-08-02 21:21:32 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const commando = require('discord.js-commando')
+const Discord = require('discord.js')
+const bot = new  commando.Client({
+  commandPrefix: 'mort:'
+ });
+ 
+ const TOKEN = 'NjA3MDE3OTgyMzQ2MDAyNDU2.XUTf7A.wMllNe-3uh9hO2alvyG4NWF8r_4'
+ 
+ bot.on('ready', function(){
+  console.log('mort is god');
+  bot.user.setActivity(`mort`, type: "WATCHING");
+  .catch(console.error);
+  });
+ 
+bot.on('guildMemberAdd', function(member)
+{
+	member.send("mort https://cdn.discordapp.com/attachments/603000331055530007/603810563574726666/unknown.png")
+}); 
+ 
+bot.login(TOKEN)
+ 
